735c16eb3543da07c7ed36203bd088dabe341816
leesh26 <dlthgml1026@naver.com>
2022-09-16 14:44:09 +0900

    (#32) chore : 프로젝트 초기 설정

---
+++ server/src/main/java/seb39_40/coffeewithme/CoffeewithmeApplication.java
@@ -0,0 +1,13 @@
+package seb39_40.coffeewithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeewithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeewithmeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/seb39_40/coffeewithme/CoffeewithmeApplicationTests.java
@@ -0,0 +1,13 @@
+package seb39_40.coffeewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeewithmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
