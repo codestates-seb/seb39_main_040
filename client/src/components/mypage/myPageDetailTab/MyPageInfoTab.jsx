55497f9037a4a44f6f4bfcdfec1580e2aa1d6514
eunyoung <shineeol20@gmail.com>
2022-09-24 11:26:37 +0900

    style: 나의 정보탭 구조 조정

--- client/src/components/mypage/myPageDetailTab/MyPageInfoTab.jsx
+++ client/src/components/mypage/myPageDetailTab/MyPageInfoTab.jsx
@@ -1,79 +1,110 @@
 import styled from "styled-components";
 
-const MainWrapper = styled.div`
+const InfoBox = styled.div`
   display: flex;
-  flex-direction: column;
+  border: 1px solid red;
+  min-width: 1080px; // 1080 - 360 = 720/2 = 360 + 180 + 180
+  height: 400px;
 `;
 
-const InfoWrapper = styled.div`
+const InfoImgBox = styled.div`
+  display: flex;
+  width: 360px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 304px;
+    height: 304px;
+    border-radius: 50%;
+  }
+  button {
+    margin-top: 20px;
+    width: 80px;
+    height: 24px;
+    border-radius: 5px;
+    background-color: #f0ece3;
+    color: #735757;
+  }
+  button:focus {
+    outline: none;
+  }
+`;
+
+const InfoDetailBox = styled.div`
+  //border: 1px solid green;
   display: flex;
   flex-direction: row;
-  //justify-content: space-between;
-  align-items: center;
-  border: 1px solid red;
-  width: 930px;
-  height: 300px;
+  width: 100%;
 `;
 
-const ProfileImg = styled.img`
-  width: 250px;
-  height: 250px;
-  border-radius: 50%;
-  margin-right: 50px;
-  margin-left: 20px;
-  //border: 1px solid green;
-`;
-
-const ProfileInfo = styled.div`
-  max-width: 580px;
-  ul {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 170px;
-  }
-  li {
-    margin-bottom: 20px;
+const InfoTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 180px;
+  height: 400px;
+  div {
+    padding: 20px 0 20px 40px;
   }
 `;
 
-const ButtonBox = styled.div`
-  //border: 1px solid red;
+const InfoDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 270px;
+  height: 400px;
+  div {
+    margin: 20px 0 20px 0;
+    border-bottom: 1px solid #757575;
+  }
+`;
+
+const ButtonBox = styled.button`
   display: flex;
   justify-content: flex-end;
-  margin-top: 40px;
+  align-items: flex-end;
+  margin-left: 72px;
   button {
-    width: 100px;
-    height: 50px;
-    margin: 10px;
+    //margin-top: 20px;
+    width: 88px;
+    height: 36px;
+    border-radius: 5px;
     background-color: #f0ece3;
-    border-radius: 10px;
     color: #735757;
-    font-weight: bold;
-    font-size: 15px;
+    margin-left: 20px;
+    margin-bottom: 24px;
   }
-  button:hover {
-    background-color: #dacaa5;
+  button:focus {
+    outline: none;
   }
 `;
+
 const MyPageInfoTab = () => {
   return (
-    <MainWrapper>
-      <InfoWrapper>
-        <ProfileImg src="https://mblogthumb-phinf.pstatic.net/MjAyMDAzMjlfMzkg/MDAxNTg1NDA4ODEzMzI2.TgYHw1rfLOzhNud2l1TQnYpBWO2C5s9gaILeSU07HLIg.jni1H76nFFFoYqBEzRZDccNAV8uLzzcxhtsvxqN7QCIg.PNG.tarkyami/%ED%95%9C%EC%86%8C%ED%9D%AC28.png?type=w800" />
-        <ProfileInfo>
-          <ul>
-            <li>이름 : 한소희</li>
-            <li>이메일 : hanxoxo@gmail.com</li>
-            <li>전화번호 : 010-XXXX-XXXX</li>
-          </ul>
-        </ProfileInfo>
-      </InfoWrapper>
-      <ButtonBox>
-        <button className="button-edit">정보수정</button>
-        <button className="button-delete">회원탈퇴</button>
-      </ButtonBox>
-    </MainWrapper>
+    <InfoBox>
+      <InfoImgBox>
+        <img src="https://mblogthumb-phinf.pstatic.net/MjAyMDAzMjlfMzkg/MDAxNTg1NDA4ODEzMzI2.TgYHw1rfLOzhNud2l1TQnYpBWO2C5s9gaILeSU07HLIg.jni1H76nFFFoYqBEzRZDccNAV8uLzzcxhtsvxqN7QCIg.PNG.tarkyami/%ED%95%9C%EC%86%8C%ED%9D%AC28.png?type=w800" />
+        <button>이미지 수정</button>
+      </InfoImgBox>
+      <InfoDetailBox>
+        <InfoTitle>
+          <div>이름</div>
+          <div>이메일</div>
+          <div>전화번호</div>
+        </InfoTitle>
+        <InfoDetail>
+          <div>한소희</div>
+          <div>hanxoxo@gmail.com</div>
+          <div>010-XXXX-XXXX</div>
+        </InfoDetail>
+        <ButtonBox>
+          <button>수정하기</button>
+          <button>회원탈퇴</button>
+        </ButtonBox>
+      </InfoDetailBox>
+    </InfoBox>
   );
 };
 
