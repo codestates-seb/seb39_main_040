29d217e52a13a1eb7b8350b9d67290a2da4e18a2
eunyoung <shineeol20@gmail.com>
2022-09-29 00:01:18 +0900

    fix: import React 버그 수정

--- client/src/components/cafe/CafeCards.jsx
+++ client/src/components/cafe/CafeCards.jsx
@@ -1,6 +1,6 @@
+import React, { useState, useEffect } from "react";
 import CafeCard from "./CafeCard";
 import styled from "styled-components";
-import { useState, useEffect } from "react";
 import axios from "axios";
 
 const CafeCardWrapper = styled.div`
--- client/src/components/review/NewReviewForm.jsx
+++ client/src/components/review/NewReviewForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
