ef7c7f24a9df62abc7e22f15e29b135bf2501cf5
eunyoung <shineeol20@gmail.com>
2022-09-29 20:40:17 +0900

    fix: 로그인 상태 저장명 변경

--- client/src/store/useLoginStore.jsx
+++ client/src/store/useLoginStore.jsx
@@ -2,12 +2,15 @@ import create from "zustand";
 import { persist } from "zustand/middleware";
 
 const useLoginStore = create(
-  persist((set) => ({
-    isLogin: false,
-    setIsLogin: () => {
-      set((state) => ({ isLogin: !state.isLogin }));
-    },
-  }))
+  persist(
+    (set) => ({
+      isLogin: false,
+      setIsLogin: () => {
+        set((state) => ({ isLogin: !state.isLogin }));
+      },
+    }),
+    { name: "isLogin" }
+  )
 );
 
 export default useLoginStore;
