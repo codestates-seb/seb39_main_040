26252ce0f5b86024ba5d4a2796999726d4ab4779
eunyoung <shineeol20@gmail.com>
2022-10-01 14:33:59 +0900

    feat: 유저리뷰 조회&삭제 구현

--- client/src/components/mypage/UserReviewCards.jsx
+++ client/src/components/mypage/UserReviewCards.jsx
@@ -1,18 +1,69 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import UserReviewCard from "./UserReviewCard";
 
 const ReviewCardWrapper = styled.div``;
 
 const UserReviewCards = () => {
+  const [reviewInfos, setReviewInfos] = useState([]);
+
+  useEffect(() => {
+    let token = localStorage.getItem("access_token");
+    axios.defaults.headers.common["AccessToken"] = `${token}`;
+    axios
+      .get(`${process.env.REACT_APP_API}/users/reviews`)
+      .then((res) => {
+        console.log(res.data.reviews);
+        setReviewInfos(res.data.reviews);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <ReviewCardWrapper>
-      <UserReviewCard />
-      <UserReviewCard />
-      <UserReviewCard />
-      <UserReviewCard />
+      {reviewInfos.length === 0 ? (
+        <MessageBox>
+          <div>작성한 리뷰가 없습니다!</div>
+        </MessageBox>
+      ) : (
+        reviewInfos.map((el) => (
+          <UserReviewCard
+            key={el.id}
+            review_id={el.id}
+            cafe_id={el.cafe.id}
+            cafe_name={el.cafe.name}
+            content={el.description}
+            star={el.score}
+            review_img={el.reviewImg}
+            tags={el.tags}
+          />
+        ))
+      )}
     </ReviewCardWrapper>
   );
 };
 
 export default UserReviewCards;
+
+const MessageBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 40vh;
+
+  div {
+    margin-bottom: 50px;
+    color: var(--gray-020);
+    font-size: 3rem;
+    //opacity: 0.6;
+  }
+  a {
+    text-decoration: none;
+    color: var(--gray-020);
+  }
+  a:hover {
+    color: var(--green-010);
+  }
+`;
--- client/src/pages/Review/EditReviewPage.jsx
+++ client/src/pages/Review/EditReviewPage.jsx
@@ -2,7 +2,10 @@ import styled from "styled-components";
 import EditReviewForm from "../../components/review/EditReviewForm";
 import Header from "../../components/common/Header";
 import MiddleTitle from "../../components/common/MiddleTitle";
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const EditReviewWrapper = styled.div`
   display: flex;
@@ -18,6 +21,18 @@ const FormContent = styled.div`
 `;
 
 const EditReviewPage = () => {
+  const [originReview, setOriginReview] = useState();
+  const { cafe_id, review_id } = useParams();
+  console.log(cafe_id, review_id);
+
+  useEffect(() => {
+    let token = localStorage.getItem("access_token");
+    axios.defaults.headers.common["AccessToken"] = `${token}`;
+    axios
+      .get(`${process.env.REACT_APP_API}/users/reviews`)
+      .then((res) => console.log(res.data.reviews));
+  }, []);
+
   return (
     <EditReviewWrapper>
       <Header />
