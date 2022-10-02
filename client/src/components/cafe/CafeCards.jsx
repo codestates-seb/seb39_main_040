a2b166e13cfcb2445bfaa515229b5e5c6522ee7b
eunyoung <shineeol20@gmail.com>
2022-10-02 14:31:56 +0900

    (#111) feat: 검색 기능 구현

--- client/src/components/cafe/CafeCards.jsx
+++ client/src/components/cafe/CafeCards.jsx
@@ -17,19 +17,49 @@ const CafeCardWrapper = styled.div`
   }
 `;
 
-const CafeCards = () => {
+const CafeCards = ({ searchInput }) => {
   const [cafeInfo, setCafeInfo] = useState([]);
+  // const { searchInput } = useStore();
+
+  const searchGet = async (param) => {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API}/cafe/search?keyword=${param}`
+    );
+    console.log(response.data.data);
+    setCafeInfo(response.data.data);
+  };
 
   // 카페 전체 리스트 불러오기
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API}/cafe`)
       .then((res) => {
-        console.log(res.data.data);
         setCafeInfo(res.data.data);
       })
       .catch((e) => console.log("error:", e));
+    if (searchInput) {
+      console.log(searchInput);
+      axios
+        .get(`${process.env.REACT_APP_API}/cafe/search?name=${searchInput}`)
+        .then((res) => {
+          console.log(res.data.data);
+        })
+        .catch((err) => console.log(err));
+    }
   }, []);
+
+  useEffect(() => {
+    // if (searchInput) {
+    //   console.log(searchInput);
+    //   axios
+    //     .get(`${process.env.REACT_APP_API}/cafe/search/keyword=${searchInput}`)
+    //     .then((res) => {
+    //       console.log(res.data.data);
+    //     })
+    //     .catch((err) => console.log(err));
+    // }
+    searchGet(searchInput);
+  }, [searchInput]);
 
   return (
     <CafeCardWrapper>
