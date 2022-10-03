419af4d369774ad4f1a69e976f4953c8c9e01ab3
eunyoung <shineeol20@gmail.com>
2022-10-03 21:05:18 +0900

    (#145) feat: 메인페이지 무한스크롤 구현

--- client/src/components/common/Loading.jsx
+++ client/src/components/common/Loading.jsx
@@ -2,32 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import Spinner from "../../assets/Spinner.gif";
 
-const Background = styled.div`
-  position: absolute;
-  width: 100vw;
-  height: 100vh;
-  top: 0;
-  left: 0;
-  background: #ffffffb7;
-  z-index: 999;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const LoadingText = styled.div`
-  font: 1rem "Noto Sans KR";
-  text-align: center;
-`;
-
 const Loading = () => {
   return (
     <Background>
-      <LoadingText>잠시만 기다려 주세요.</LoadingText>
-      <img src={Spinner} alt="loading" width="5%" />
+      <img src={Spinner} alt="로딩이미지" />
     </Background>
   );
 };
 
 export default Loading;
+
+const Background = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
